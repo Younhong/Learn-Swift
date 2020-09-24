3499172cc6f9ec6a2ad913b276ceb1d9f69c499f
younhong <younhong@kakao.com>
2020-09-24 10:30:38 +0900

    alarm mission

---
+++ AlarmClock/AlarmClock/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  AlarmClock
+//
+//  Created by Younhong Ko on 2020/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var alarmTime: String?
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblPickerTime: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm aaa"
+        
+        alarmTime = formatter.string(from: datePickerView.date)
+        
+        lblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+    }
+    
+    @objc func updateTime() {
+        let date = NSDate()
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = "hh:mm aaa"
+        let currentTime = formatter.string(from: date as Date)
+        lblCurrentTime.text = "현재시간: " + currentTime
+        
+        if (alarmTime == currentTime) {
+            view.backgroundColor = UIColor.red
+        } else {
+            view.backgroundColor = UIColor.white
+        }
+    }
+    
+}
+
