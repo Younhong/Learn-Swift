8d6fcc74f244b4bc7e066123865e4042c9230c40
younhong <younhong@kakao.com>
2020-09-27 17:54:38 +0900

    step2

--- Audio/Audio/ViewController.swift
+++ Audio/Audio/ViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    var audioPlayer: AVAudioPlayer!
+    var audioFile: URL!
+    let MAX_VOLUME: Float = 10.0
+    var progressTimer: Timer!
+    
+    @IBOutlet var pvProgressPlay: UIProgressView!
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblEndTime: UILabel!
+    @IBOutlet var btnPlay: UIButton!
+    @IBOutlet var btnPause: UIButton!
+    @IBOutlet var btnStop: UIButton!
+    @IBOutlet var slVolume: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        audioFile = Bundle.main.url(forResource: "Sicilian_Breeze", withExtension: "mp3")
+        initPlay()
     }
-
-
+    
+    func initPlay() {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioFile)
+        } catch let error as NSError {
+            print("Error-initPlay : \(error)")
+        }
+        slVolume.maximumValue = MAX_VOLUME
+        slVolume.value = 1.0
+        pvProgressPlay.progress = 0
+        audioPlayer.delegate = self
+        audioPlayer.prepareToPlay()
+        audioPlayer.volume = slVolume.value
+        
+    }
+    
+    @IBAction func btnPlayAudio(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnPauseAudio(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnStopAudio(_ sender: UIButton) {
+    }
+    
+    @IBAction func slChangeVolume(_ sender: UISlider) {
+    }
+    
 }
 
