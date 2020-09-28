c5a318caf45fb9cc7290914cb8c97b1b9763e029
younhong <younhong@kakao.com>
2020-09-28 10:09:08 +0900

    record mode

--- Audio/Audio/ViewController.swift
+++ Audio/Audio/ViewController.swift
@@ -8,11 +8,12 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, AVAudioPlayerDelegate {
+class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
     var audioPlayer: AVAudioPlayer!
     var audioFile: URL!
     let MAX_VOLUME: Float = 10.0
     var progressTimer: Timer!
+    let timePlayerSelector:Selector = #selector(ViewController.updatePlayTime)
     
     @IBOutlet var pvProgressPlay: UIProgressView!
     @IBOutlet var lblCurrentTime: UILabel!
@@ -22,11 +23,70 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     @IBOutlet var btnStop: UIButton!
     @IBOutlet var slVolume: UISlider!
     
+    @IBOutlet var btnRecord: UIButton!
+    @IBOutlet var lblRecordTime: UILabel!
+    
+    var audioRecorder: AVAudioRecorder!
+    var isRecordMode = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        audioFile = Bundle.main.url(forResource: "Sicilian_Breeze", withExtension: "mp3")
-        initPlay()
+        
+        selectAudioFile()
+        if !isRecordMode {
+            initPlay()
+            btnRecord.isEnabled = false
+            lblRecordTime.isEnabled = false
+        } else {
+            initRecord()
+        }
+    }
+    
+    func selectAudioFile(){
+        if !isRecordMode {
+            audioFile = Bundle.main.url(forResource: "Sicilian_Breeze", withExtension: "mp3")
+        } else {
+            let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            audioFile = documentDirectory.appendingPathComponent("recordFile.m4a")
+        }
+    }
+    
+    func initRecord() {
+        let recordSettings = [
+            AVFormatIDKey: NSNumber(value: kAudioFormatAppleLossless as UInt32),
+            AVEncoderAudioQualityKey: AVAudioQuality.max.rawValue,
+            AVEncoderBitRateKey: 320000,
+            AVNumberOfChannelsKey: 2,
+            AVSampleRateKey: 44100.0
+        ] as [String:Any]
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFile, settings: recordSettings)
+        } catch let error as NSError {
+            print("Error-initRecord : \(error)")
+        }
+        audioRecorder.delegate = self
+        
+        slVolume.value = 1.0
+        audioPlayer.volume = slVolume.value
+        lblEndTime.text = convertNSTimeInterval2String(0)
+        lblCurrentTime.text = convertNSTimeInterval2String(0)
+        setPlayButtons(false, pause: false, stop: false)
+        
+        let sessions = AVAudioSession.sharedInstance()
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch let error as NSError {
+            print("Error-setCategory: \(error)")
+        }
+        do {
+            try sessions.setActive(true)
+        } catch let error as NSError {
+            print("Error-setActive: \(error)")
+        }
+        
     }
     
     func initPlay() {
@@ -42,18 +102,85 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         audioPlayer.prepareToPlay()
         audioPlayer.volume = slVolume.value
         
+        lblCurrentTime.text = convertNSTimeInterval2String(0)
+        lblEndTime.text = convertNSTimeInterval2String(audioPlayer.duration)
+        
+        btnPlay.isEnabled = true
+        btnStop.isEnabled = false
+        btnPause.isEnabled = false
+        
+        setPlayButtons(true, pause: false, stop: false)
+    }
+    
+    func setPlayButtons(_ play:Bool, pause:Bool, stop:Bool) {
+        btnPlay.isEnabled = play
+        btnPause.isEnabled = pause
+        btnStop.isEnabled = stop
+    }
+    
+    func convertNSTimeInterval2String(_ time:TimeInterval) ->String {
+        let min = Int(time/60)
+        let sec = Int(time.truncatingRemainder(dividingBy: 60))
+        let strTime = String(format: "%02d:%02d", min, sec)
+        return strTime
     }
     
     @IBAction func btnPlayAudio(_ sender: UIButton) {
+        audioPlayer.play()
+        setPlayButtons(false, pause: true, stop: true)
+        progressTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: timePlayerSelector, userInfo: nil, repeats: true)
+    }
+    
+    @objc func updatePlayTime() {
+        lblCurrentTime.text = convertNSTimeInterval2String(audioPlayer.currentTime)
+        pvProgressPlay.progress = Float(audioPlayer.currentTime/audioPlayer.duration)
     }
     
     @IBAction func btnPauseAudio(_ sender: UIButton) {
+        audioPlayer.pause()
+        setPlayButtons(true, pause: false, stop: true)
     }
     
     @IBAction func btnStopAudio(_ sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.currentTime = 0
+        lblCurrentTime.text = convertNSTimeInterval2String(0)
+        setPlayButtons(true, pause: false, stop: false)
+        progressTimer.invalidate()
     }
     
     @IBAction func slChangeVolume(_ sender: UISlider) {
+        audioPlayer.volume = slVolume.value
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        progressTimer.invalidate()
+        setPlayButtons(true, pause: false, stop: false)
+    }
+    
+    @IBAction func swRecordMode(_ sender: UISwitch) {
+        if sender.isOn {
+            audioPlayer.stop()
+            audioPlayer.currentTime = 0
+            lblRecordTime!.text = convertNSTimeInterval2String(0)
+            isRecordMode = true
+            btnRecord.isEnabled = true
+            lblRecordTime.isEnabled = true
+        } else {
+            isRecordMode = false
+            btnRecord.isEnabled = false
+            lblRecordTime.isEnabled = false
+            lblRecordTime.text = convertNSTimeInterval2String(0)
+        }
+        selectAudioFile()
+        if !isRecordMode {
+            initPlay()
+        } else {
+            initRecord()
+        }
+    }
+    
+    @IBAction func btnRecord(_ sender: UIButton) {
     }
     
 }
